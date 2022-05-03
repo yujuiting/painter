549ffe2b530021a7e0e3944a862d5b09ec7ddb97
yujuiting <yujuiting@me.com>
2022-05-03 15:53:48 +0800

    prebuild script

---
+++ scripts/prebuild.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathToRemove = [
+  path.resolve(process.cwd(), "node_modules/roughjs/bundled/rough.js"),
+  path.resolve(
+    process.cwd(),
+    "node_modules/@types/react-color/node_modules/react"
+  ),
+  path.resolve(
+    process.cwd(),
+    "node_modules/@types/reactcss/node_modules/react"
+  ),
+];
+
+for (const path of pathToRemove) {
+  fs.rmSync(path, { force: true, recursive: true });
+}
